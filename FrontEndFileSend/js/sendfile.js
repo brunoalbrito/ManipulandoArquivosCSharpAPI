7ccc3ba3e6a819357d767737a40a19ebe2b6d0fc
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2018-12-31 14:53:53 -0200

    Arrumando metodo para enviar arquivo e esta funcionando

--- FrontEndFileSend/js/sendfile.js
+++ FrontEndFileSend/js/sendfile.js
@@ -1,10 +1,14 @@
 $("#sendFile").click(function(){
     var formData = new FormData();
-    var file = $("#inputFile")[0];
+    var inputFile = $("#inputFile")[0];
+    formData.append("file", inputFile.files[0])
+
     $.ajax({
         url: 'http://localhost:49829/api/data/receiveFile',
         data: formData,
         type: 'POST',
+        enctype: 'multipart/form-data',
+        dataType: 'json',
         contentType: false,
         processData: false,
         success:function(data){
