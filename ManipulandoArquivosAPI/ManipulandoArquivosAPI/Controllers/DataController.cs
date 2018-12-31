9be651c2d5ca09ee3e47d1e99920ec76930df80c
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2018-12-31 14:52:59 -0200

    Exibindo a primeira linha do arquivo

--- ManipulandoArquivosAPI/ManipulandoArquivosAPI/Controllers/DataController.cs
+++ ManipulandoArquivosAPI/ManipulandoArquivosAPI/Controllers/DataController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
@@ -17,6 +21,15 @@ namespace ManipulandoArquivosAPI.Controllers
         [Route("receiveFile")]
         public HttpResponseMessage receiveFile()
         {
+            var httpRequest = HttpContext.Current.Request;
+
+            var file = httpRequest.Files["file"];
+
+            StreamReader stream = new StreamReader(file.InputStream, Encoding.Default);
+            var line = stream.ReadLine();
+
+            Debug.WriteLine(line);
+
             return Request.CreateResponse(HttpStatusCode.Accepted,
                 "Inserção realizada com sucesso");
         }
