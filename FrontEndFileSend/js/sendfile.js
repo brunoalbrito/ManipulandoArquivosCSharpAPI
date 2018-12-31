72a7da09af2f121e6b2e8a23620b3e0afdae563c
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2018-12-31 14:07:44 -0200

    Envio de arquivo funcionando corretamente

--- FrontEndFileSend/js/sendfile.js
+++ FrontEndFileSend/js/sendfile.js
@@ -1,3 +1,17 @@
-$("#sendFile").click(()=>{
-    alert("Funcionou")
-})
+$("#sendFile").click(function(){
+    var formData = new FormData();
+    var file = $("#inputFile")[0];
+    $.ajax({
+        url: 'http://localhost:49829/api/data/receiveFile',
+        data: formData,
+        type: 'POST',
+        contentType: false,
+        processData: false,
+        success:function(data){
+            alert("Ocorreu com sucesso")
+        },
+        error: function(){
+            alert("Ocorreu uma falha ao inserir")
+        }
+    })  
+});
