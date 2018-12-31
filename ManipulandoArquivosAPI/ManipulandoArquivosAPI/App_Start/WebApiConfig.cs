672f7e018fe147cad09dd6f82e17ef90493a2f29
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2018-12-31 14:07:25 -0200

    Adicionando EnableCors para permitir a requisição com a mesmo origem

--- ManipulandoArquivosAPI/ManipulandoArquivosAPI/App_Start/WebApiConfig.cs
+++ ManipulandoArquivosAPI/ManipulandoArquivosAPI/App_Start/WebApiConfig.cs
@@ -14,6 +14,8 @@ namespace ManipulandoArquivosAPI
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            config.EnableCors();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
--- ManipulandoArquivosAPI/ManipulandoArquivosAPI/Controllers/DataController.cs
+++ ManipulandoArquivosAPI/ManipulandoArquivosAPI/Controllers/DataController.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ManipulandoArquivosAPI.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/data")]
     public class DataController : ApiController
     {
 
